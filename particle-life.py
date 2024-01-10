b62cb93d26676cd90d647a474953c65d81a5b19e
AnselmoJimenez <anselmojimenez0@gmail.com>
2024-01-10 05:32:51 -0500

    Particles appear on screen

--- particle-life.py
+++ particle-life.py
@@ -1,10 +1,6 @@
 import pygame
 import random
 
-# Constants
-NUM_COLORS = 3
-NUM_PARTICLES = 30
-
 class Particle:
     def __init__(self, x: float, y: float, velocityX: float, velocityY: float, color: list[int]) -> None:
         self.x:float = x
@@ -17,33 +13,59 @@ class Particle:
         return f"Particle: {self.color} at Location {self.x, self.y}"
 
 
-def generate_particles():
-    particles = []
+class Simulation:
+    number_of_colors = 3
+    number_of_particles = 30
+    height = 1024
+    width = 1024
 
-    # Determine how many particles are allocated to each color
-    particles_per_color = NUM_PARTICLES // NUM_COLORS
+    def __init__(self) -> None:
+        self.particles = self.generate_particles()
+        self.mainloop()
+        
+    def generate_particles(self) -> list[Particle]:
+        particles = []
 
-    # Generate the colors based on the number of colors and fill with random RGB value
-    colors_index = [[] for i in range(NUM_COLORS)]
-    for color in colors_index:
-        for i in range(3):
-            color.append(random.randint(0, 255))
+        # Determine how many particles are allocated to each color
+        particles_per_color = self.number_of_particles // self.number_of_colors
 
-    # Generate all particles and assign color and random location
-    for i in range(NUM_PARTICLES):
-        color = colors_index[i // particles_per_color]
-        x = random.random()
-        y = random.random()
+        # Generate the colors based on the number of colors and fill with random RGB value
+        colors_index = [[] for i in range(self.number_of_colors)]
+        for color in colors_index:
+            for i in range(3):
+                color.append(random.randint(0, 255))
 
-        particles.append(Particle(x, y, 0, 0, color))
+        # Generate all particles and assign color and random location
+        for i in range(self.number_of_particles):
+            color = colors_index[i // particles_per_color]
+            x = random.random()
+            y = random.random()
 
+            particles.append(Particle(x, y, 0, 0, color))
 
-def main():
-    # generate all particles and assign them all a color and location
-    particles = generate_particles(particles)
+        return particles
+    
+    def mainloop(self) -> None:
+        # Display the Window
+        window = pygame.display.set_mode((self.width, self.height), pygame.RESIZABLE)
+        pygame.display.set_caption("Particle-Life")
 
-    for p in particles:
-        print(p)
+        run = True
+        FPS = 60
+        clock = pygame.time.Clock()
 
-if __name__ == "__main__":
-    main()
+        while run:
+            clock.tick(FPS)
+
+            for particle in self.particles:
+                scaled_x = (particle.x * 1024) // 1
+                scaled_y = (particle.y * 1024) // 1
+                pygame.draw.circle(window, particle.color, (scaled_x, scaled_y), 5, 0)
+
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: run = False
+            
+
+Simulation()
