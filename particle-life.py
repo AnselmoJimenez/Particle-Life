0f6bdb7c9ecbeac203ae5e993d880da6b3d7ea80
AnselmoJimenez <anselmojimenez0@gmail.com>
2024-01-12 06:31:13 -0500

    particles interact well but get stuck or disappear at edge of screen

--- particle-life.py
+++ particle-life.py
@@ -16,26 +16,26 @@ class Particle:
 
 class Simulation:
     def __init__(self) -> None:
-        self.width = 1024
-        self.height = 1024
+        self.width = 1500
+        self.height = 1500
         self.number_of_colors = 3
-        self.number_of_particles = 30
+        self.number_of_particles = 100
 
-        self.delta_time = 0.2
-        self.repulsion_distance = 0.3
+        self.delta_time = 0.02
+        self.friction_force = 0.2
 
         self.attraction_matrix = self.generate_attraction_matrix()
         self.particles, self.color_dictionary = self.generate_particles()
-        print(self.color_dictionary)
+
         self.mainloop()
     
      # Go through each color and give it an attraction based on randomness
     def generate_attraction_matrix(self) -> list[list[int]]:
-        matrix = [[] for i in range(self.number_of_colors)]
+        matrix = [[] for _ in range(self.number_of_colors)]
 
         for color in matrix:
-            for i in range(self.number_of_colors):
-                color.append(random.random())
+            for _ in range(self.number_of_colors):
+                color.append(random.uniform(-1, 1))
 
         return matrix
 
@@ -51,7 +51,12 @@ class Simulation:
 
         # Generate all particles and assign color and random location
         for i in range(self.number_of_particles):
-            color = colors_index[i // particles_per_color]
+            # may get an index error on the last particle
+            try:
+                color = colors_index[i // particles_per_color]
+            except IndexError as e:
+                color = colors_index[i // particles_per_color - 1]
+
             x = random.random()
             y = random.random()
 
@@ -63,27 +68,44 @@ class Simulation:
     
     # # Go through each particle, compare it against all other particles, update accordingly
     def update_particles(self) -> None:
-        for particle_0 in self.particles:
-            for particle in self.particles:
-                if particle_0 is particle: continue
+        # Update accelerations and velocities
+        for particle1 in self.particles:
+            acceleration_X = 0
+            acceleration_Y = 0
+
+            for particle2 in self.particles:
+                if particle1 is particle2: continue
 
                 # Calculate distance between the particle
-                rx = (particle.x - particle_0.x)**2
-                ry = (particle.y - particle_0.y)**2
-                r = math.sqrt(rx + ry)
-                if r < self.repulsion_distance: continue
+                rx = particle2.x - particle1.x
+                ry = particle2.y - particle1.y
+                r = math.hypot(rx, ry)
+
+                # Calculate the accelerations of each particle based on the distance to other particles and the attraction matrix
+                repulsion_distance = 0.2
+                attraction = self.attraction_matrix[self.color_dictionary[particle1.color]][self.color_dictionary[particle2.color]]
+                if r < repulsion_distance: 
+                    attraction_force = (r / repulsion_distance) - 1
+                elif repulsion_distance < r and r < 1: 
+                    attraction_force = attraction * (1 - abs(2 * r - 1 - repulsion_distance) / (1 - repulsion_distance))
+                else:
+                    attraction_force = 0
 
                 # Update accelerations of each particle
-                attraction = self.attraction_matrix[self.color_dictionary[particle_0.color]]
-                acceleration_X = (attraction * rx) / r**3
-                acceleration_Y = (attraction * ry) / r**3
+                acceleration_X += attraction_force * rx / r
+                acceleration_Y += attraction_force * ry / r
 
-                # Update the velocities based on the accelerations of each particle
-                particle_0.velocityX = particle_0.velocityX + acceleration_X * self.delta_time
-                particle_0.velocityY = particle_0.velocityY + acceleration_Y * self.delta_time
+            # Update the velocities based on the accelerations of each particle
+            particle1.velocityX *= self.friction_force
+            particle1.velocityY *= self.friction_force    
 
-            particle_0.x = particle_0.velocityX * self.delta_time
-            particle_0.y = particle_0.velocityY * self.delta_time
+            particle1.velocityX += particle1.velocityX + acceleration_X * self.delta_time
+            particle1.velocityY += particle1.velocityY + acceleration_Y * self.delta_time
+
+        # Update positions based on velocities
+        for particle in self.particles:
+            particle.x = (particle.x + particle.velocityX * self.delta_time) % self.width
+            particle.y = (particle.y + particle.velocityY * self.delta_time) % self.height
 
     
     def mainloop(self) -> None:
@@ -103,16 +125,16 @@ class Simulation:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: run = False
 
-            # Reset display each update
+            # Reset display each update and update the particles
             window.fill((0, 0, 0))
 
             for particle in self.particles:
                 scaled_x = (particle.x * self.width) // 1
                 scaled_y = (particle.y * self.height) // 1
-                pygame.draw.circle(window, particle.color, (scaled_x, scaled_y), 5, 0)
+                pygame.draw.circle(window, particle.color, (scaled_x, scaled_y), 3, 0)
 
-            pygame.display.update()
             self.update_particles()
+            pygame.display.update()
             
 
 Simulation()
